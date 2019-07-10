55b9a3a51055032ed240d1580fedc6411e02475e
gitgiter <1176748429@qq.com>
2019-07-10 21:47:25 +0800

    update ConvNCF

--- ConvNCF.py
+++ ConvNCF.py
@@ -4,9 +4,40 @@ import torch.nn as nn
 import torch.utils.data as Data
 import torch.optim as optim
 from torch.autograd import Variable
+import torch.multiprocessing as mp
 import math
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
+def show_curve(ys, title):
+    """plot curlve for Loss and Accuacy
+    Args:
+        ys: loss or acc list
+        title: Loss or Accuracy
+    """
+    x = np.array(range(len(ys)))
+    y = np.array(ys)
+    plt.plot(x, y, c='b')
+    plt.axis()
+    plt.title('{} Curve:'.format(title))
+    plt.xlabel('Epoch')
+    plt.ylabel('{} Value'.format(title))
+    plt.show()
+
+def show_metric(ys, title):
+    """plot curlve for HR and NDCG
+    Args:
+        ys: hr or ndcg list
+        title: HR@k or NDCG@k
+    """
+    x = np.array(range(len(ys)))
+    y = np.array(ys)
+    plt.plot(x, y, c='b')
+    plt.axis()
+    plt.title('{} Curve:'.format(title))
+    plt.xlabel('User')
+    plt.ylabel('{} Value'.format(title))
+    plt.show()
 
 class ConvNCF(nn.Module):
 
@@ -59,7 +90,7 @@ class ConvNCF(nn.Module):
 #         self.drop_prop = 0.5
 #         self.dropout = nn.Dropout(drop_prop)
 
-    def forward(self, user_ids, item_ids):
+    def forward(self, user_ids, item_ids, is_pretrain):
 
         # convert float to int
         user_ids = list(map(int, user_ids))
@@ -70,21 +101,28 @@ class ConvNCF(nn.Module):
 #         item_embeddings = self.Q[item_ids]
         user_embeddings = self.P(torch.tensor(user_ids).cuda())
         item_embeddings = self.Q(torch.tensor(item_ids).cuda())
+        
+#         # inner product
+#         prediction = torch.sum(torch.mul(user_embeddings, item_embeddings), dim=1)
+            
+        if is_pretrain:
+            # inner product
+            prediction = torch.sum(torch.mul(user_embeddings, item_embeddings), dim=1)
+        else:
+            # outer product
+            # interaction_map = torch.ger(user_embeddings, item_embeddings) # ger is 1d
+            interaction_map = torch.bmm(user_embeddings.unsqueeze(2), item_embeddings.unsqueeze(1))
+            interaction_map = interaction_map.view((-1, 1, self.embedding_size, self.embedding_size))
 
-        prediction = torch.sum(torch.mul(user_embeddings, item_embeddings), dim=1)
+            # cnn
+            feature_map = self.cnn(interaction_map)  # output: batch_size * 32 * 1 * 1
+            feature_vec = feature_map.view((-1, 32))
 
-#         # outer product
-#         # interaction_map = torch.ger(user_embeddings, item_embeddings) # ger is 1d
-#         interaction_map = torch.bmm(user_embeddings.unsqueeze(2), item_embeddings.unsqueeze(1))
-#         interaction_map = interaction_map.view((-1, 1, self.embedding_size, self.embedding_size))
+            # fc
+            prediction = self.fc(feature_vec)
+            prediction = prediction.view((-1))
 
-#         # cnn
-#         feature_map = self.cnn(interaction_map)  # output: batch_size * 32 * 1 * 1
-#         feature_vec = feature_map.view((-1, 32))
-
-#         # fc
-#         prediction = self.fc(feature_vec)
-
+#         print('is_pretrain:', is_pretrain, prediction.shape)
         return prediction
 
 
@@ -96,11 +134,10 @@ class BPRLoss(nn.Module):
 
     def forward(self, pos_preds, neg_preds):
         distance = pos_preds - neg_preds
-        regular = 0
-        # loss = torch.sum(torch.log(self.sigmoid(-distance)), dim=1) + regular
-#         loss = torch.sum(torch.log((1 + torch.exp(-distance))), dim=1) + regular
-        loss = torch.sum(torch.log((1 + torch.exp(-distance)))) + regular
-        return torch.mean(loss)
+        loss = torch.sum(torch.log((1 + torch.exp(-distance))))
+        
+#         print('loss:', loss)
+        return loss
 
 
 def train():
@@ -111,29 +148,48 @@ def train():
     accuracies = []
 
     model.train()
-    train_loader = Data.DataLoader(yelp.train_group, batch_size=batch_size, shuffle=True, num_workers=4)
+    
     optimizer = optim.Adagrad(model.parameters(), lr=lr, weight_decay=1e-2)
-    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.2, last_epoch=-1)
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=50, gamma=0.1, last_epoch=-1)
 
+    train_loader = Data.DataLoader(yelp.train_group, batch_size=batch_size, shuffle=True, num_workers=4)
+    
     bpr_loss = BPRLoss().cuda()
 
-    for epoch in range(epoches):        
+    for epoch in range(epoches):    
+#         yelp.train_group = yelp.resample_train_group()
+#         train_loader = Data.DataLoader(yelp.train_group, batch_size=batch_size, shuffle=True, num_workers=4)
+        
         total_loss = 0
         total_acc = 0
         try:
             with tqdm(train_loader) as t:
-                for batch_idx, train_data in enumerate(t):
+                for batch_idx, train_data in enumerate(t):                    
         #             train_data = Variable(train_data.cuda())
                     user_ids = Variable(train_data[:, 0].cuda())
                     pos_item_ids = Variable(train_data[:, 1].cuda())
                     neg_item_ids = Variable(train_data[:, 2].cuda())
+                    
                     optimizer.zero_grad()
-                    pos_preds = model(user_ids, pos_item_ids)
-                    neg_preds = model(user_ids, neg_item_ids)
-                    loss = bpr_loss(pos_preds, neg_preds)
-                    total_loss += loss.item()
+                    
+#                     # pretrain bpr
+#                     pos_preds = model(user_ids, pos_item_ids, True)
+#                     neg_preds = model(user_ids, neg_item_ids, True) 
+
+                    if epoch < 100:    
+                        # pretrain bpr
+                        pos_preds = model(user_ids, pos_item_ids, True)
+                        neg_preds = model(user_ids, neg_item_ids, True)                        
+                    else:
+                        # train convncf                        
+                        pos_preds = model(user_ids, pos_item_ids, False)
+                        neg_preds = model(user_ids, neg_item_ids, False)
+                        
+                    loss = bpr_loss(pos_preds, neg_preds)                    
+                    total_loss += loss.item()                    
                     loss.backward()
                     optimizer.step()
+                    
                     accuracy = float(((pos_preds - neg_preds) > 0).sum()) / float(len(pos_preds))
                     total_acc += accuracy
         #             if batch_idx % 20 == 0:
@@ -149,7 +205,7 @@ def train():
             show_curve(losses, 'train loss')
             show_curve(accuracies, 'train acc')
             evaluate()
-        if epoch == 4:
+        if epoch == 3 or epoch == 20:
             evaluate()
         scheduler.step()
               
@@ -164,13 +220,12 @@ def evaluate():
     ndcg20 = []
 
     user_count = len(yelp.test_negative)
-#     user_count = 200
     try:
         with tqdm(range(user_count)) as t:
             for u in t:
                 item_ids = torch.tensor(yelp.test_negative[u]).cuda()
                 user_ids = torch.tensor([u] * len(item_ids)).cuda()
-                predictions = model(user_ids, item_ids)
+                predictions = model(user_ids, item_ids, False)
                 topv, topi = torch.topk(predictions, 20, dim=0)
                 hr, ndcg = scoreK(topi, 5)
                 hr5.append(hr)
@@ -216,8 +271,9 @@ def scoreK(topi, k):
     # auc = 1 - (position * 1. / negs)
     return hr, ndcg#, auc
 
+
 if __name__ == '__main__':
-    torch.set_num_threads(12)
+    # torch.set_num_threads(12)
     print('Data loading...')
     yelp = load_dataset.Load_Yelp('./Data/yelp.train.rating', './Data/yelp.test.rating', './Data/yelp.test.negative')
     print('Data loaded')
@@ -240,4 +296,3 @@ if __name__ == '__main__':
     print('Model evaluating...')
     evaluate()
     print('Model evaluated')
-
